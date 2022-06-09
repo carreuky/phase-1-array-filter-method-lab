2b8247f783b04fce0252d26d6b79f78207e903b2
carreuky <carreuky@gmail.com>
2022-06-09 20:35:31 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+
+// making two comparisions
+function findMatching( driver, passednamed){
+    return (driver.filter(driver=> driver.toLowerCase() === passednamed.toLowerCase()))
+}
+function fuzzyMatch(driver, passedNamed) {
+    return (driver.filter(driver=>driver.toLowerCase().indexOf(passedNamed.toLowerCase())===0))
+    
+}function matchName(driver ,passedName) {
+    return (driver.filter(driver=>driver.name===passedName))
+}
